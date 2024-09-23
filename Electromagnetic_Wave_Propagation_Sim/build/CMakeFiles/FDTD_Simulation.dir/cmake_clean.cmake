55eda86e34c8b0d114867d0a0eb49e52a88da349
Dave <dave@dave.com>
2024-09-23 14:34:44 -0700

    Updating README -> FDTD project

---
+++ Electromagnetic_Wave_Propagation_Sim/build/CMakeFiles/FDTD_Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FDTD_Simulation.dir/main.cxx.o"
+  "CMakeFiles/FDTD_Simulation.dir/main.cxx.o.d"
+  "bin/FDTD_Simulation"
+  "bin/FDTD_Simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FDTD_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
